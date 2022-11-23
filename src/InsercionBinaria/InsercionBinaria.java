51e4a03ff07afab10e11ff6bb3127d42cc78a9e0
Abrajam7611 <luis.vel4x@gmail.com>
2022-11-22 21:17:40 -0600

    Proyecto InsersionBinario

---
+++ src/InsercionBinaria/InsercionBinaria.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package InsercionBinaria;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Abraham
+ */
+public class InsercionBinaria {
+	public static void main(String[] args) {
+		
+		int[] num = {11,45,67,27,89,84,65,21,44};
+		int numinterse =45;
+		
+		Arrays.sort(num);
+		
+/* Si está en el array la respuesta es la posición del número en un array ordenado
+* si no devuelve el número de elementos que ha tenido que recorrer hasta buscarlo 
+*/
+		
+		if (Arrays.binarySearch(num, numinterse)>0)
+			System.out.println("El número " + numinterse + " está en el array");
+		else 
+			System.out.println("El número " + numinterse + " no está en el array");
+
+	}
+
+}
